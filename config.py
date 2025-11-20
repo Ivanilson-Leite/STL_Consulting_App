2474a656cd6f8fea30263c5bc3cb8e89459fca57
Ivanilson Leite <ivanilsonguga@gmail.com>
2025-11-20 07:35:27 -0300

    v.1.0.1

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ class Config:
     # Adicione ?charset=utf8mb4 ao final do nome do banco
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL',
-        'mysql+pymysql://root:Est%40020234@localhost:3306/sch_stl?charset=utf8mb4'
+        # 'mysql+pymysql://root:Est%40020234@localhost:3306/sch_stl?charset=utf8mb4'
+        'mysql+pymysql://root:stlbanco@localhost:3306/sch_stl?charset=utf8mb4'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'uma_chave_secreta_muito_forte_para_sessoes')
